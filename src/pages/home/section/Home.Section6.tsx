cb8ff9b2277653ab8cecc77525ed6af94f152689
Thiago Medeiros <thiagotitico1@gmail.com>
2026-01-08 19:59:14 -0300

    Fazendo ajustes na seção 6 e deixando mais responsiva para o desktop

--- src/pages/home/section/Home.Section6.tsx
+++ src/pages/home/section/Home.Section6.tsx
@@ -9,21 +9,21 @@ export default function Home_Section6() {
                 <div className="Home_Section6_localizacao_cards_Palestras">
                     <h6>Palestras</h6>
                     <h5>Oceania Park Hotel Spa & Convention Center</h5>
-                    <p>14 a 19 de Outubro</p>
+                    <p className="Home_Section6_localizacao_cards_Palestras_data">14 a 19 de Outubro</p>
                     <img src={hotel} alt="" />
                     <p>Dias principais do evento, espaços para job fair, network, palestras de diversos temas acontecendo simultaneamente.</p>
-                    <div>
+                    <div className="Home_Section6_localizacao_cards_Palestras_buttons">
                         <a href=""><button>Planta do Evento</button></a>
                         <a href=""><button>Ver no mapa</button></a>
                     </div>
                 </div>
                 <div className="Home_Section6_localizacao_cards_Tutoriais">
                     <h6>Tutoriais</h6>
-                    <p>A definir</p>
+                    <h5>A definir</h5>
                 </div>
                 <div className="Home_Section6_localizacao_cards_Sprints">
                     <h6>Sprints</h6>
-                    <p>A definir</p>
+                    <h5>A definir</h5>
                 </div>
             </div>
             
